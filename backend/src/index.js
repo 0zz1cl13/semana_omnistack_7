6d7847f611c86ebb809ba7a5784e2b00134833b1
0zz1cl13 <seniorcl13@gmail.com>
2019-12-30 19:01:30 -0400

    finalização da api

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
 
 const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 mongoose.connect('mongodb://localhost:27017/omnistack_7', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-app.get('/', (req, res) => {
-  return res.send(`Hello dd ${req.query.name}`);
+app.use((req, res, next) => {
+  req.io = io;
+  next();
 });
 
-app.listen(3333);
+app.use(cors());
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
+
+app.use(require('./routes'));
+
+server.listen(3333);
